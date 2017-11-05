e984fb2acf095700fdf4f009d187967b80d9ae1e
Rizwan Aslam(Am) <rizwan@codebrisk.com>
2017-11-06 00:46:43 +0500

    fixe some more left links

--- app/Http/Controllers/Admin/LocationController.php
+++ app/Http/Controllers/Admin/LocationController.php
@@ -7,5 +7,25 @@ use App\Http\Controllers\Controller;
 
 class LocationController extends Controller
 {
-    //
+
+
+    public function indexRegions()
+    {
+        $regions = '';
+        return view('admin.location.index-regions' , compact('regions'));
+    }
+
+
+    public function indexCountries()
+    {
+        $countries= '';
+        return view('admin.location.index-countries' , compact('countries'));
+    }
+
+    public function indexCities()
+    {
+        $cities = '';
+        return view('admin.location.index-cities' , compact('cities'));
+    }
+
 }
