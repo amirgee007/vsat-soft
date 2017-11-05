29b9a7b137555d01e057e416e0f68f606b797c04
Rizwan Aslam(Am) <rizwan@codebrisk.com>
2017-11-05 17:25:31 +0500

    install "laravelcollective/html" package for better laravel forms

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         PrettyRoutes\ServiceProvider::class,
         Arcanedev\LogViewer\LogViewerServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -228,6 +229,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
